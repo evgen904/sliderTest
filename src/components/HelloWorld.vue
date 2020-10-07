2e2c81a643ef1807732598780b7d7dcfd27b80d7
Evgen <evgen904@gmail.com>
2020-10-07 17:58:07 +0400

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,199 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Slider</h1>
+    <br><br><br><br><br><br>
+    <br><br><br><br><br><br>
+    <br><br><br><br><br><br>
+
+
+    <div class="slider-new-wrap">
+      <div
+        ref="sliderNew"
+        class="slider-new"
+        @touchstart="touchStart"
+        @touchmove="touchMove"
+        @touchend="touchEnd"
+      >
+        <div v-for="(slide, i) in countSlider" :key="i">
+          <div class="bg" :class="`bg-${i}`"></div>
+        </div>
+      </div>
+      <div class="slider-count">{{ activeSlide }} / {{ countSlider }}</div>
+    </div>
+
+
+
+
+
+    <br><br><br><br><br><br>
+    <br><br><br><br><br><br>
+    <br><br><br><br><br><br>
+    <br><br><br><br><br><br>
+    <br><br><br><br><br><br>
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      text: '',
+      activeSlide: 1,
+      countSlider: 4,
+      sliderWidth: 0
+    }
+  },
+  mounted() {
+     this.sliderWidth = this.$refs.sliderNew.scrollWidth / this.countSlider
+    document.querySelector('.slider-new').addEventListener('scroll', () => {
+      console.log(this.$refs.sliderNew.scrollLeft)
+      if(this.$refs.sliderNew.scrollLeft > 0) {
+        this.$refs.sliderNew.scrollLeft = (0 + this.sliderWidth) * this.activeSlide;
+      }
+    })
+  },
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  methods: {
+    touchStart() {
+      //console.log('touchStart')
+    },
+    touchMove() {
+      //console.log('touchMove')
+    },
+    touchEnd(event) {
+      //console.log('touchEnd')
+      this.activeSlide = Math.round(this.$refs.sliderNew.scrollLeft / this.sliderWidth) + 1
+
+
+
+      let posTouchX = event.changedTouches[0].clientX - this.$refs.sliderNew.getBoundingClientRect().left
+      let procX = Math.round(posTouchX / this.sliderWidth * 100)
+
+
+      if (procX > 50) {
+        //console.log('right')
+      } else {
+        //console.log('left')
+      }
+
+      // this.$refs.sliderNew.scrollLeft = this.sliderWidth * (this.activeSlide-1)
+      // console.log(this.$refs.sliderNew.scrollLeft)
+
+
+
+      // this.$refs.sliderNew.scrollBy({
+      //   top: 0,
+      //   left: 280,
+      //   behavior: 'smooth'
+      // })
+
+
+
+
+
+
+
+
+
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.hello {
+  h1 {
+    color: #cc0000;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+
+
+.slider-new-wrap {
+  position: relative;
+  width: 280px;
+  margin: 0 auto;
+
+  .slider-count {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background: #fff;
+    color: #000000;
+    font-size: 14px;
+    padding: 12px;
+    z-index: 100;
+  }
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.slider-new {
+  display: flex;
+  flex-wrap: nowrap;
+  width: 100%;
+
+  //scroll-snap-type: x mandatory;
+  overflow-x: scroll;
+  scroll-behavior: smooth;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    width: 0px;
+    background: transparent;
+    display: none;
+  }
+
+  > div {
+    min-width: 100%;
+    height: 100%;
+    flex-grow: 1;
+    scroll-snap-align: start;
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+      pointer-events: unset;
+    }
+  }
 }
-a {
-  color: #42b983;
-}
+
+
+
+  .bg {
+    height: 200px;
+    &.bg-0 {
+      background: #f7d3d3;
+    }
+    &.bg-1 {
+      background: #d3f7e3;
+    }
+    &.bg-2 {
+      background: #eed3f7;
+    }
+    &.bg-3 {
+      background: #d3ebf7;
+    }
+    &.bg-4 {
+      background: #f3d3f7;
+    }
+    &.bg-5 {
+      background: #d3def7;
+    }
+  }
+
+
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { VueHammer } from 'vue2-hammer'
+Vue.use(VueHammer)
 
 Vue.config.productionTip = false
 
